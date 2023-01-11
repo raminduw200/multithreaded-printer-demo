5a1a3d8977252a1ec451073229069618a45c8d04
Ramindu Walgama <raminduw200@gmail.com>
2023-01-11 08:13:18 +0530

    Implemnet Student and Toner Technician thread

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class Student {
+public class Student extends Thread {
+    private final LaserPrinter printer;
+    private final Logger logger;
+//    Have private data members that hold the information associated with him/her,
+//    i.e. the thread group he/she is in; his/her printer; his/her name.
+
+    public Student(ThreadGroup group, LaserPrinter printer, String studentName) {
+        super(group, studentName);
+        this.printer = printer;
+        this.logger = Logger.getLogger();
+    }
+
+    /*
+    • Create and print five documents with different lengths and names.
+    • He/she should "sleep" for a random amount of time between each printing request.
+    • When he/she has finished printing, print out a message, e.g.
+    Joe Bloggs Finished Printing: 5 Documents, 95 pages
+     */
+    @Override
+    public void run() {
+        Document[] documents = new Document[5];
+        int numberOfDocuments = 0;
+        /*
+        Document CWK2 = new Document( "JoeBloggs", "cwk2", 20 ); printer.printDocument( CWK2 ) ;
+         */
+        documents[0] = new Document(
+                this.getName(), "Concurrent Programming - Coursework I", Utility.randomDocumentSize());
+        documents[1] = new Document(
+                this.getName(), "Concurrent Programming - Coursework II", Utility.randomDocumentSize());
+        documents[2] = new Document(
+                this.getName(), "Finite Automata - Assignment II", Utility.randomDocumentSize());
+        documents[3] = new Document(
+                this.getName(), "Operating System - Semaphore is OS", Utility.randomDocumentSize());
+        documents[4] = new Document(
+                this.getName(),
+                "Data Structures and Algorithms - Booyer More Algorithm",
+                Utility.randomDocumentSize()
+        );
+
+        for(Document document: documents){
+            logger.printDocumentStatus(printer, document);
+            printer.printDocument(document);
+            if (printer.isDocumentPrinted()) {
+                numberOfDocuments++;
+                logger.printPrinterStatus(printer, "DOCUMENT PRINTED");
+            } else {
+                logger.printPrinterStatus(printer, "ERROR: DOCUMENT PRINTED FAILED");
+            }
+
+            try {
+                // sleep thread for a random amount of time between 1 and 100 milliseconds (inclusive)
+                sleep(Utility.randomThreadSleepTime()/10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        logger.printThreadFinishStatus(this, numberOfDocuments, "DOCUMENTS");
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + this.getName() + " ] : ";
+    }
 }
--- src/TonerTechnician.java
+++ src/TonerTechnician.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class TonerTechnician {
+public class TonerTechnician extends Thread {
+    private final ServicePrinter printer;
+    private final Logger logger;
+
+    public TonerTechnician(ThreadGroup group, ServicePrinter printer, String technicianName) {
+        super(group, technicianName);
+        this.printer = printer;
+        this.logger = Logger.getLogger();
+    }
+
+    /*
+    This class is very similar to the paper technician class, except it attempts to replace
+     the toner three times, using the printer's replaceTonerCartridge( ) method.
+     When he/she has finished trying to replace the toner cartridge, print out a message,
+     stating it has finished and how many toner cartridges it replaced, e.g.
+    Toner Technician Finished, cartridges replaced: 2
+     */
+    @Override
+    public void run() {
+        int numberOfReplace = 0;
+        for(int i=0; i < 3; i++){
+            logger.printRefillingStatus(printer, "TONER", Thread.currentThread().getName());
+            printer.replaceTonerCartridge();
+            if (((LaserPrinter) printer).isTonerReplaced()) {
+                numberOfReplace ++;
+                logger.printPrinterStatus(printer, "TONER REFILLED");
+            } else {
+                logger.printPrinterStatus(printer, "ERROR: TONER REFILLED FAILED");
+            }
+            try {
+                // sleep thread for a random amount of time between 1 and 100 milliseconds (inclusive)
+                sleep(Utility.randomThreadSleepTime());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // Paper Technician Finished
+        logger.printThreadFinishStatus(this, numberOfReplace, "TONER CARTRIDGES REPLACED");
+    }
+
+    @Override
+    public String toString() {
+        return "[ PAPER_TECHNICIAN : " + this.getName() + " ]";
+    }
 }
