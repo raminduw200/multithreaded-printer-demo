cc48ba421412fe45dac5bec59f2dfb3de6aa44f5
Ramindu Walgama <raminduw200@gmail.com>
2023-01-12 05:22:34 +0530

    Generate document pages between given range

--- src/Utility.java
+++ src/Utility.java
@@ -1,11 +1,14 @@
 public class Utility {
     // generate random document size(pages count) between 1 and 10 (inclusive)
     public static int randomDocumentSize() {
-        return (int) (Math.random() * 10 + 1);
+        int min = 1;
+        int max = 10;
+        int range = max - min + 1;
+        return (int) (Math.random() * range) + min;
     }
 
     // sleep thread for a random amount of time between 1 and 1000 milliseconds;1 second (inclusive)
     public static int randomThreadSleepTime() {
-        return (int) (Math.random() * 1000 + 1);
+        return (int) (Math.random() * 100 + 1);
     }
 }
